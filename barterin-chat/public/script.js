50a26782f81c901ddbb43094dda30cf11249957d
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-02-13 17:28:17 +0700

    update script.js - remove some function and comment - change variable name socket to sc

--- barterin-chat/public/script.js
+++ barterin-chat/public/script.js
@@ -1,40 +1,24 @@
-const socket = io('/')
+const sc = io('/')
 $(document).ready(function() {
     let segment = document.URL.split('/').slice(3);
     console.log(segment)
     let userId = segment[0]
-    $("#userId").html(userId.split('-')[0]);
-    socket.emit('connected', segment)
+    let userIdTarget = segment[1]
+    $("#userId").html(userId);
+    $("#userIdTarget").html(userIdTarget);
+    sc.emit('connected', segment)
 
     $("#formStoreMessage").on('submit', function(e) {
         e.preventDefault()
         let message = $("#message").val()
+        const messageId = Date.now()
+        let date = new Date()
         if (message.length == 0) return
-        socket.emit('sendMessage', segment, message.trim())
-        $("#message").val('')
-    })
-
-    socket.on('receiveMessage', (userId, message, date) => {
-        // if (userId != segment[1]) return
-        // let html = `
-        //     <div class="card p-2 d-flex flex-row align-items-center shadow">
-        //         <div class="img-profile-user border rounded-circle ml-2 mr-3">
-        //             <img src="/dist/img/AdminLTELogo.png" alt="">
-        //         </div>
-        //         <div class="d-flex flex-column">
-        //             <div class="user">
-        //                 <b>${userId}</b> - ${date}
-        //             </div>
-        //             <div class="message">
-        //                 ${message}
-        //             </div>
-        //         </div>
-        //     </div>
-        // `
         let html = `
             <div class="message-in mx-3 my-2 p-2">
                 <img src="/img/user2.jpg" class="img-responsive rounded-circle"
-                    style="height: 30px; width: 30px; object-fit: cover"> <b>${userId}</b> - ${date}
+                    style="height: 30px; width: 30px; object-fit: cover"> <b>${userId}</b> - ${ date.toLocaleString('en') } <i
+                        id="${messageId}" class="fas fa-clock"></i> 
                 <p>
                     ${message}
                 </p>
@@ -42,31 +26,37 @@ $(document).ready(function() {
         `
         $("#chatContent").append(html)
         document.getElementById('chatContent').scrollTo(0,document.getElementById('chatContent').scrollHeight);
+        sc.emit('sendMessage', {
+            sender: userId,
+            receiver: userIdTarget,
+            messageId: messageId,
+            message: message.trim(),
+            date: date.toLocaleString('en')
+        })
+        $("#message").val('')
     })
 
-    $("#message").on('blur', function() {
-        $(this).val($(this).val().replace(/[^@_.a-zA-Z0-9]/g, ''))
+    sc.on('receiveMessage', (data) => {
+        let html = `
+            <div class="message-in mx-3 my-2 p-2">
+                <img src="/img/user2.jpg" class="img-responsive rounded-circle"
+                    style="height: 30px; width: 30px; object-fit: cover"> <b>${data.sender}</b> - ${data.date} 
+                <p>
+                    ${data.message}
+                </p>
+            </div>
+        `
+        $("#chatContent").append(html)
+        document.getElementById('chatContent').scrollTo(0,document.getElementById('chatContent').scrollHeight);
     })
 
-    // socket.on('user-connected', userId => {
-    //     console.log("user connected", userId);
-
-    //     let html = `
-    //         <li class="nav-item">
-    //             <a href="">
-    //                 <div class="user-panel pt-2 pb-2 d-flex listUser" data-username="user1" id="user-user1">
-    //                     <div class="image"> <img src="http://chatapp.test/assets/img/user.png" class="img-circle elevation-2"
-    //                         alt="User Image"> </div>
-    //                     <div class="info"> <span class="d-block">${userId}</span> </div>
-    //                 </div>
-    //             </a>
-    //         </li>
-    //     `
-    //     userId != segment[0] && $("#userList").append(html)
-    // })
+    sc.on('messageSend', (messageId) => {
+        const message = $(`#${messageId}`)
+        message.attr('class', 'fas fa-check')
+    })
 })
 
 
-socket.on('user-disconnected', userId => {
+sc.on('user-disconnected', userId => {
     console.log("user disconnected", userId);
 })
