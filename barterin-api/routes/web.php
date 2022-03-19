a84782cc9bda5dafa3293f06bbb6a8edf3ac9e45
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-03-19 19:09:30 +0700

    router changes - add auth to group - add testing router

--- barterin-api/routes/web.php
+++ barterin-api/routes/web.php
@@ -13,9 +13,28 @@
 |
 */
 
-$router->post('auth/login', 'AuthController@login');
-$router->post('auth/register', 'AuthController@register');
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'auth'], function ($router) {
+    $router->post('/login', 'AuthController@login');
+    $router->post('/register', 'AuthController@register');
+    $router->post('/refresh', 'AuthController@refresh');
+    $router->get('/user-profile', 'AuthController@userProfile');
+});
+
+$router->group(['middleware' => 'auth', 'prefix' => 'user'], function ($router) {
+    $router->get('/test', 'UsersController@test');
+});
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+// ], function ($router) {
+//     Route::post('/login', [AuthController::class, 'login']);
+//     Route::post('/register', [AuthController::class, 'register']);
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::post('/refresh', [AuthController::class, 'refresh']);
+//     Route::get('/user-profile', [AuthController::class, 'userProfile']);
+// });
