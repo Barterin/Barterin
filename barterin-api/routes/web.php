061dba88742446b7a00a8e1a3642d356eb3d7a0f
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-04-13 01:23:05 +0700

    update routes

--- barterin-api/routes/web.php
+++ barterin-api/routes/web.php
@@ -21,11 +21,19 @@ $router->group(['prefix' => 'auth'], function ($router) {
     $router->post('/login', 'AuthController@login');
     $router->post('/register', 'AuthController@register');
     $router->post('/refresh', 'AuthController@refresh');
-    $router->get('/user-profile', 'AuthController@userProfile');
+    $router->post('/logout', 'AuthController@logout');
+    $router->post('/user-profile', 'AuthController@userProfile');
+    $router->post('/send-email', 'MailController@sendEmailVerification');
+    $router->post('/verify-email', 'MailController@verifyEmail');
 });
 
 $router->group(['middleware' => 'auth', 'prefix' => 'user'], function ($router) {
     $router->get('/test', 'UsersController@test');
+});
+
+$router->group(['prefix' => 'test'], function ($router) {
+    $router->post('send-email', 'MailController@sendEmailVerification');
+    // $router->post('send-email', 'MailController@mail');
 });
 
 // Route::group([
