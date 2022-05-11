0c536df48ad001202f8d28a5efd4cb9cef27140e
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-05-11 16:25:38 +0700

    add Address model

---
+++ barterin-api/app/Models/Address.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'address';
+
+    protected $fillable = [
+        "user_id",
+        "penerima",
+        "nohp",
+        "label",
+        "kota_kecamatan",
+        "alamat_lengkap",
+        "kode_pos"
+    ];
+}
