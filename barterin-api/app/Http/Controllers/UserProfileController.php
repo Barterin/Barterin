85465b537713cc7e73c062b81d29de2136dd566f
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-05-19 17:14:00 +0700

    add uploadPhotoProfile function

--- barterin-api/app/Http/Controllers/UserProfileController.php
+++ barterin-api/app/Http/Controllers/UserProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use App\Models\UserProfile;
 
@@ -46,6 +47,57 @@ class UserProfileController extends Controller
             $response = [
                 "statusCode" => 200,
                 "message" => "Profile berhasil diupdate"
+            ];
+        } catch (\Throwable | \Exception $error) {
+            $response = [
+                "statusCode" => GetStatusCode($error),
+                "message" => $error->getMessage(),
+                "input" => $validator->errors()
+            ];
+        } finally {
+            return response()->json(
+                $response,
+                $response["statusCode"]
+            );
+        }
+    }
+
+    public function uploadPhotoProfile(Request $request)
+    {
+        try {
+
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    "photo" => "required|image"
+                ],
+                [
+                    "required" => "Tidak ada file yang dipilih",
+                    "image" => "Format gambar tidak valid"
+                ]
+            );
+
+            if ($validator->fails())
+                throw new \Exception('Kolom inputan tidak sesuai, periksa kembali kolom inputan anda', 400);
+
+            // get latest profile picture
+            $user = UserProfile::where(["user_id" => $this->userData->id])->first();
+            // if exists then delete
+            if ($user && $user->profile_picture != null)
+                File::delete("uploads/images/profiles/" . $user->profile_picture);
+
+            $file = $request->photo;
+            $fileName = md5(time());
+            $extension = $file->extension();
+
+            $file->move("uploads/images/profiles", $fileName . "." . $extension);
+
+            UserProfile::updateOrInsert(["user_id" => $this->userData->id], ["profile_picture" => $fileName . "." . $extension]);
+
+            $response = [
+                "statusCode" => 200,
+                "message" => "Photo profile berhasil diubah",
+                // "path" => env("APP_URL") . "/uploads/images/profiles/$fileName.$extension",
             ];
         } catch (\Throwable | \Exception $error) {
             $response = [
