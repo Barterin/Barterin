39a5cb1c33c132c97519344cb2889c9b95cf2298
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-05-23 19:07:56 +0700

    head changes - change getenv -> base_url - add meta environment - load js file based on environment

--- barterin-web/app/Views/frontend/layouts/head.php
+++ barterin-web/app/Views/frontend/layouts/head.php
@@ -5,9 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="base-url" content="<?= getenv('app.baseURL') ?>">
+    <meta name="base-url" content="<?= base_url() ?>">
     <meta name="api-url" content="<?= getenv('app.apiUrl') ?>">
     <meta name="socket-url" content="<?= getenv('app.socketUrl') ?>">
-    <script defer src="<?= getenv('app.baseURL') ?>/assets/js/app.js"></script>
+    <meta name="environment" content="<?= getenv('CI_ENVIRONMENT') ?>">
+    <script defer
+        src="<?= base_url() ?>/assets/js/app<?= getenv('CI_ENVIRONMENT') == "development" ? '' : '.min' ?>.js?<?= time() ?>">
+    </script>
     <title class="webTitle"><?= $title ?? "Barterin" ?></title>
 </head>
