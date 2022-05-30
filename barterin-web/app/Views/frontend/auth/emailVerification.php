78dd46981cdae9b8101c22377485d6f2c622e0e8
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-05-30 21:07:54 +0700

    fix email verification view

--- barterin-web/app/Views/frontend/auth/emailVerification.php
+++ barterin-web/app/Views/frontend/auth/emailVerification.php
@@ -8,25 +8,31 @@
     </div>
     <div class="containerRight">
         <div class="font signup">
-            Not a member? <a href="<?php $base_url?>/auth/register">Sign Up</a>
+            Not a member? <a href="<?php $base_url ?>/auth/register" class="internal">Sign Up</a>
         </div>
         <div class="form">
             <form id="formVerification">
                 <div class="font title">
                     Email Verification
                 </div>
+                <label><?= session()->getFlashdata("message") ?></label>
                 <label class="mt-3" for="email"></label>
                 <div class="form-body">
                     <div class="group-name">
                         <div class="form-group">
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code1" autofocus>
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code2">
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code3">
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code4">
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code5">
-                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number" id="code6">
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code1" autofocus>
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code2">
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code3">
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code4">
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code5">
+                            <input class="form-control mt-1 field-name shadow-none" maxlength="1" type="number"
+                                id="code6">
                         </div>
-                            <input class="form-control mt-1 verify_code" type="hidden" id="verify_code" name="verify_code" readonly>
                     </div>
                     <input class="btn btn-submit" type="submit" value="Submit">
                 </div>
