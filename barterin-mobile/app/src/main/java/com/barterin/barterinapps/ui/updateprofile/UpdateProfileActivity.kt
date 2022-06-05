633792953675d60f7a327a1c7a2a0a2dd04ba924
Dicky Arya Pratama <dickyarya.personal@gmail.com>
2022-06-05 18:12:07 +0700

    add edit profile feature
    add edit profile feature with costumized camera system

--- barterin-mobile/app/src/main/java/com/barterin/barterinapps/ui/updateprofile/UpdateProfileActivity.kt
+++ barterin-mobile/app/src/main/java/com/barterin/barterinapps/ui/updateprofile/UpdateProfileActivity.kt
@@ -28,6 +28,9 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import com.barterin.barterinapps.data.Result
+import com.barterin.barterinapps.ui.bottomnavigation.HomeActivity
+import com.barterin.barterinapps.ui.emailverification.EmailVerificationActivity
+import com.bumptech.glide.Glide
 
 class UpdateProfileActivity : AppCompatActivity() {
 
@@ -37,27 +40,27 @@ class UpdateProfileActivity : AppCompatActivity() {
 
     private var getFile: File? = null
 
-//    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-//        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-//    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-//            if (!allPermissionsGranted()) {
-//                Toast.makeText(
-//                    this,
-//                    "Tidak mendapatkan permission.",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                finish()
-//            }
-//        }
-//    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,165 +68,270 @@ class UpdateProfileActivity : AppCompatActivity() {
         _binding = ActivityUpdateProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        if (!allPermissionsGranted()) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                REQUIRED_PERMISSIONS,
-//                REQUEST_CODE_PERMISSIONS
-//            )
-//        }
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
 
-//        setupView()
-    }
+        setupView()
 
 
-//        binding.btnChooseImage.setOnClickListener {
-////            registerForContextMenu(binding.btnChooseImage)
-//            intentGallery()
-//        }
-//
-//        getData()
-//
-//        binding.btnUpdate.setOnClickListener {
-//
-//            uploadImage()
-//
-//        }
+        binding.btnChooseImage.setOnClickListener {
+            registerForContextMenu(binding.btnChooseImage)
+        }
+
+        getData()
+
+        binding.btnUpdate.setOnClickListener {
+
+            uploadImage()
+
+        }
 
     }
 
-//    private fun uploadImage() {
-//
-//        val factory = ViewModelFactory.getInstance(this)
-//        val viewModel = ViewModelProvider(this, factory)[UpdateProfileViewModel::class.java]
-//        sharedpref = SharedPreferenceClass(this)
-//
-//        if (getFile != null) {
-//            val file = reduceFileImage(getFile as File)
-//
-//            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
-//            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
-//                "photo",
-//                file.name,
-//                requestImageFile
-//            )
-//
-//            viewModel.uploadProfilePhoto(sharedpref.getToken(), imageMultipart).observe(this) {result ->
-//
-//                if (result != null) {
-//                    when (result) {
-//                        is Result.Loading -> {
-//                        }
-//                        is Result.Success -> {
-//                            Toast.makeText(
-//                                this,
-//                                "Success",
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-//
-//                        }
-//                        is Result.Error -> {
-//                            Toast.makeText(
-//                                this,
-//                                resources.getString(R.string.text_error) + result.error,
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-//                        }
-//                    }
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-//
-//    private fun getData() {
-//        sharedpref = SharedPreferenceClass(this)
-//        binding.txtName.text = sharedpref.getName()
-//        binding.txtUsername.text = sharedpref.getUsername()
-//        binding.txtEmail.text = sharedpref.getEmail()
-//    }
-//
-//    override fun onCreateContextMenu(
-//        menu: ContextMenu?,
-//        v: View?,
-//        menuInfo: ContextMenu.ContextMenuInfo?
-//    ) {
-//        super.onCreateContextMenu(menu, v, menuInfo)
-//        menuInflater.inflate(R.menu.floating_camera_menu, menu)
-//    }
-//
-//    override fun onContextItemSelected(item: MenuItem): Boolean {
-//
-//        when(item.itemId) {
-//            R.id.item_camera -> intentCameraX()
-//            R.id.item_galery -> intentGallery()
-//        }
-//        return super.onContextItemSelected(item)
-//    }
-//
-//    private fun intentCameraX() {
-//        val intent = Intent(this, CameraActivity::class.java)
-//        launcherIntentCameraX.launch(intent)
-//    }
-//
-//    private val launcherIntentCameraX = registerForActivityResult(
-//        ActivityResultContracts.StartActivityForResult()
-//    ) {
-//        if (it.resultCode == CAMERA_X_RESULT) {
-//            val myFile = it.data?.getSerializableExtra("picture") as File
-//            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
-//
-//            getFile = myFile
-//            val result = rotateBitmap(
-//                BitmapFactory.decodeFile(getFile?.path),
-//                isBackCamera
-//            )
-//            binding.imgProfile.setImageBitmap(result)
-//        }
-//    }
-//
-//    private fun intentGallery() {
-//        val intent = Intent()
-//        intent.action = Intent.ACTION_GET_CONTENT
-//        intent.type = "image/*"
-//        val chooser = Intent.createChooser(intent, "Choose a Picture")
-//        launcherIntentGallery.launch(chooser)
-//    }
-//
-//    private val launcherIntentGallery = registerForActivityResult(
-//        ActivityResultContracts.StartActivityForResult()
-//    ) { result ->
-//        if (result.resultCode == RESULT_OK) {
-//            val selectedImg: Uri = result.data?.data as Uri
-//
-//            val myFile = uriToFile(selectedImg, this@UpdateProfileActivity)
-//
-//            getFile = myFile
-//            val resultImage = BitmapFactory.decodeFile(getFile?.path)
-////            binding.imgProfile.setImageBitmap(resultImage)
-//        }
-//    }
-//
-//    private fun setupView() {
-//
-//        @Suppress("DEPRECATION")
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            window.insetsController?.hide(WindowInsets.Type.statusBars())
-//        } else {
-//            window.setFlags(
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN
-//            )
-//        }
-//        supportActionBar?.hide()
-//    }
-//
-//    companion object {
-//        const val CAMERA_X_RESULT = 200
-//        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-//        private const val REQUEST_CODE_PERMISSIONS = 10
-//    }
-//
-//}
+    private fun uploadImage() {
+
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this, factory)[UpdateProfileViewModel::class.java]
+        sharedpref = SharedPreferenceClass(this)
+
+        if (getFile != null) {
+            val file = reduceFileImage(getFile as File)
+
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            viewModel.uploadProfilePhoto(sharedpref.getToken(), imageMultipart).observe(this) {result ->
+
+                if (result != null) {
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.progressBar6.visibility = View.VISIBLE
+                        }
+                        is Result.Success -> {
+                            Toast.makeText(
+                                this,
+                                "Success",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            uploadData()
+
+                        }
+                        is Result.Error -> {
+                            binding.progressBar6.visibility = View.INVISIBLE
+                            Toast.makeText(
+                                this,
+                                resources.getString(R.string.text_error) + result.error,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun uploadData() {
+
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this, factory)[UpdateProfileViewModel::class.java]
+        sharedpref = SharedPreferenceClass(this)
+
+        val name = binding.edtName.text.toString()
+        val phoneNumber = binding.edtPhoneNumber.text.toString()
+        val born = binding.edtBorn.text.toString()
+        val gender = binding.edtGender.text.toString()
+
+        viewModel.updateDataProfile(sharedpref.getToken(), name, phoneNumber, born, gender).observe(this) {result ->
+
+            if (result != null) {
+                when (result) {
+                    is Result.Loading -> {
+                        binding.progressBar6.visibility = View.VISIBLE
+                    }
+                    is Result.Success -> {
+                        Toast.makeText(
+                            this,
+                            "Success",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        updatePreferences()
+
+                    }
+                    is Result.Error -> {
+                        binding.progressBar6.visibility = View.INVISIBLE
+                        Toast.makeText(
+                            this,
+                            "upload data error" + result.error,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+
+    private fun updatePreferences() {
+
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this, factory)[UpdateProfileViewModel::class.java]
+        sharedpref = SharedPreferenceClass(this)
+
+        viewModel.getDataUser(sharedpref.getToken()).observe(this) { result ->
+            if (result != null) {
+                when(result) {
+                    is Result.Loading -> {
+                        binding.progressBar6.visibility = View.VISIBLE
+                    }
+                    is Result.Success -> {
+                        binding.progressBar6.visibility = View.GONE
+
+                        val userId = result.data.id
+                        val username = result.data.username
+                        val email = result.data.email
+                        val fullName = result.data.fullname
+                        val verifiedEmail = result.data.verified_email
+                        val verifiedAccount = result.data.verified_account
+                        val phone = result.data.phone
+                        val born = result.data.born
+                        val profilePicture = result.data.profile_picture
+                        val gender = result.data.gender
+
+                        sharedpref = SharedPreferenceClass(this)
+                        sharedpref.saveDataUser(userId, username, email, fullName, verifiedEmail, verifiedAccount, phone, born, profilePicture, gender, true)
+
+                        binding.progressBar6.visibility = View.GONE
+                        Toast.makeText(
+                            this,
+                            "Data Berhasil Di Update",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                    is Result.Error -> {
+                        binding.progressBar6.visibility = View.GONE
+                        Toast.makeText(
+                            this,
+                            "getdataerror" + result.error,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    private fun getData() {
+        sharedpref = SharedPreferenceClass(this)
+
+        Glide.with(this)
+            .load("https://api.barterin.tech/uploads/images/profiles/${sharedpref.getProfilePicture()}")
+            .placeholder(R.drawable.placeholder)
+            .into(binding.imgProfile)
+
+        binding.edtName.setText(sharedpref.getName())
+        binding.edtPhoneNumber.setText(sharedpref.getPhoneNumber())
+        binding.edtBorn.setText(sharedpref.getBorn())
+        binding.edtGender.setText(sharedpref.getGender())
+
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.floating_camera_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.item_camera -> intentCameraX()
+            R.id.item_galery -> intentGallery()
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    private fun intentCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            getFile = myFile
+            val result = BitmapFactory.decodeFile(getFile?.path)
+            binding.imgProfile.setImageBitmap(result)
+        }
+    }
+
+    private fun intentGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+
+            val myFile = uriToFile(selectedImg, this@UpdateProfileActivity)
+
+            getFile = myFile
+            val resultImage = BitmapFactory.decodeFile(getFile?.path)
+            binding.imgProfile.setImageBitmap(resultImage)
+        }
+    }
+
+    private fun setupView() {
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+}
+
--- barterin-mobile/app/src/main/java/com/barterin/barterinapps/ui/updateprofile/UpdateProfileViewModel.kt
+++ barterin-mobile/app/src/main/java/com/barterin/barterinapps/ui/updateprofile/UpdateProfileViewModel.kt
@@ -10,4 +10,14 @@ class UpdateProfileViewModel(private val barterinRepository: BarterinRepository)
     fun uploadProfilePhoto(token: String, file: MultipartBody.Part) =
         barterinRepository.uploadImage(token, file)
 
+    fun updateDataProfile(
+        token: String,
+        fullname: String,
+        phone: String,
+        born: String,
+        gender: String
+    ) = barterinRepository.editProfile(token, fullname, phone, born, gender)
+
+    fun getDataUser(token: String) = barterinRepository.getDataUser(token)
+
 }
